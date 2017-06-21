b6fe381566a70e3f8beb9a38342aac28483a53b4
Ethan Jackwitz <ethanjackwitz@gmail.com>
2017-06-22 00:31:14 +1000

    Cleaned up the builtin types

--- Sources/cte/Builtin.swift
+++ Sources/cte/Builtin.swift
@@ -1,43 +1,51 @@
 
-func declareBuiltins() {
+struct BuiltinType {
 
-    Entity.void.flags.insert(.type)
-    Entity.bool.flags.insert(.type)
-    Entity.type.flags.insert(.type)
-    Entity.string.flags.insert(.type)
-    Entity.number.flags.insert(.type)
+    var entity: Entity
+    var type: Type
 
-    Entity.void.type = Type.makeMetatype(Type.void)
-    Entity.bool.type = Type.makeMetatype(Type.bool)
-    Entity.type.type = Type.makeMetatype(Type.type)
-    Entity.string.type = Type.makeMetatype(Type.string)
-    Entity.number.type = Type.makeMetatype(Type.number)
+    init(name: String, width: Int) {
 
-    Scope.global.insert(Entity.void)
-    Scope.global.insert(Entity.bool)
-    Scope.global.insert(Entity.type)
-    Scope.global.insert(Entity.string)
-    Scope.global.insert(Entity.number)
+        entity = Entity.makeBuiltin(name)
+        type = Type.makeBuiltin(entity, width: width)
+
+        entity.flags.insert(.type)
+        entity.type = Type.makeMetatype(type)
+
+        Scope.global.insert(entity)
+    }
+
+    static let void = BuiltinType(name: "void", width: 0)
+    static let type = BuiltinType(name: "type", width: 64)
+    static let bool = BuiltinType(name: "bool", width: 1)
+
+    static let string = BuiltinType(name: "string", width: 64)
+    static let number = BuiltinType(name: "number", width: 64)
+
+    static let u8 = BuiltinType(name: "u8", width: 8)
 }
 
 extension Entity {
 
-    static let void = Entity.makeBuiltin("void")
-    static let bool = Entity.makeBuiltin("bool")
-    static let type = Entity.makeBuiltin("type")
-    static let string = Entity.makeBuiltin("string")
-    static let number = Entity.makeBuiltin("number")
+    static let void = BuiltinType.void.entity
+    static let type = BuiltinType.type.entity
+    static let bool = BuiltinType.bool.entity
+    static let string = BuiltinType.string.entity
+    static let number = BuiltinType.number.entity
+    static let u8 = BuiltinType.u8.entity
 
     static let anonymous = Entity.makeBuiltin("_")
 }
 
 extension Type {
 
-    static let void = Type.makeBuiltin(Entity.void, width: 0)
-    static let bool = Type.makeBuiltin(Entity.bool, width: 1)
-    static let type = Type.makeBuiltin(Entity.type, width: 64)
-    static let string = Type.makeBuiltin(Entity.string, width: 64)
-    static let number = Type.makeBuiltin(Entity.number, width: 64)
+    static let void = BuiltinType.void.type
+    static let type = BuiltinType.type.type
+    static let bool = BuiltinType.bool.type
+    static let string = BuiltinType.string.type
+    static let number = BuiltinType.number.type
+
+    static let u8 = BuiltinType.u8.type
 
     static let invalid = Type.makeBuiltin(Entity.anonymous, width: 0)
 
