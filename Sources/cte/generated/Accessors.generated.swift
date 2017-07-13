vdka/cte

commit edeea16a6317d073b7e55987abd9467b461e0fa4

No need for checked versions of Switch and Assign

Sources/cte/generated/Accessors.generated.swift
@@ -985,7 +985,6 @@ extension AstNode.Return: CommonReturn {}
 extension AstNode.StringLiteral: CommonStringLiteral {}
 extension AstNode.Switch: CommonSwitch {}
 extension AstNode.Variadic: CommonVariadic {}
-extension Checker.Assign: CommonAssign {}
 extension Checker.Block: CommonBlock {}
 extension Checker.Call: CommonCall {}
 extension Checker.Case: CommonCase {}
@@ -1002,7 +1001,6 @@ extension Checker.Paren: CommonParen {}
 extension Checker.PointerType: CommonPointerType {}
 extension Checker.Prefix: CommonPrefix {}
 extension Checker.StringLiteral: CommonStringLiteral {}
-extension Checker.Switch: CommonSwitch {}
 
 extension Type {
 

Sources/cte/Checker.swift
@@ -175,8 +175,6 @@ extension Checker {
                 reportError("Cannot assign value of type '\(lvalueType!)' to value of type '\(rvalueType)'", at: rvalue)
             }
 
-            node.value = Assign(lvalues: assign.lvalues, rvalues: assign.rvalues)
-
         case .parameter:
             let param = node.asParameter
 
@@ -338,8 +336,6 @@ extension Checker {
                 reportError("A `switch` statement must have a default block\n    Note: try adding `case:` block", at: node)
                 return
             }
-
-            node.value = Switch(subject: świtch.subject, cases: checkedCases)
 
         case .return:
             let ret = node.asReturn
@@ -1323,13 +1319,6 @@ extension Checker {
         let rhsCast: OpCode.Cast?
     }
 
-    struct Assign: CheckedAstValue {
-        typealias UncheckedValue = AstNode.Assign
-
-        let lvalues: [AstNode]
-        let rvalues: [AstNode]
-    }
-
     struct Call: CheckedExpression, CheckedAstValue {
         typealias UncheckedValue = AstNode.Call
 
@@ -1373,13 +1362,6 @@ extension Checker {
         let stmts: [AstNode]
         var isForeign: Bool
         let scope: Scope
-    }
-
-    struct Switch: CheckedAstValue {
-        typealias UncheckedValue = AstNode.Switch
-
-        let subject: AstNode?
-        let cases: [AstNode]
     }
 
     struct Case: CheckedAstValue {

Sources/cte/IRGenerator.swift
@@ -147,7 +147,7 @@ struct IRGenerator {
             }
 
         case .assign:
-            let assign = node.asCheckedAssign
+            let assign = node.asAssign
 
             if assign.rvalueIsCall && assign.lvalues.count > 1 {
                 let call = assign.rvalues[0].asCheckedCall
@@ -272,7 +272,7 @@ struct IRGenerator {
             }
 
         case .switch:
-            let świtch = node.asCheckedSwitch
+            let świtch = node.asSwitch
             if świtch.subject == nil {
                 emitBooleanesqueSwitch(świtch)
             } else {
@@ -546,7 +546,7 @@ struct IRGenerator {
         builder.positionAtEnd(of: loopPost)
     }
 
-    func emitSwitch(_ świtch: Checker.Switch) {
+    func emitSwitch(_ świtch: CommonSwitch) {
         let subject = świtch.subject!
 
         let switchLn = subject.tokens.first!.start.line
@@ -598,7 +598,7 @@ struct IRGenerator {
         builder.positionAtEnd(of: postBlock)
     }
 
-    func emitBooleanesqueSwitch(_ świtch: Checker.Switch) {
+    func emitBooleanesqueSwitch(_ świtch: CommonSwitch) {
         let switchLn = świtch.cases.first!.tokens.first!.start.line
 
         let currentFunc = builder.currentFunction!

Sources/cte/generated/Copy.generated.swift
@@ -183,12 +183,6 @@ extension AstValue {
                 cCompatible: value.cCompatible
         )
 
-        case let value as Checker.Assign:
-            return Checker.Assign(
-                lvalues: value.lvalues.map({ $0.copy() }),
-                rvalues: value.rvalues.map({ $0.copy() })
-        )
-
         case let value as Checker.Block:
             return Checker.Block(
                 stmts: value.stmts.map({ $0.copy() }),
@@ -325,12 +319,6 @@ extension AstValue {
             return Checker.StringLiteral(
                 value: value.value,
                 type: value.type.copy()
-        )
-
-        case let value as Checker.Switch:
-            return Checker.Switch(
-                subject: value.subject?.copy(),
-                cases: value.cases.map({ $0.copy() })
         )
 
         default:

commit 1f4d4c0560f6b66dcc67433a2e5e169480a0a83c

Fix compilation errors introduced in edeea16

Sources/cte/generated/Accessors.generated.swift
@@ -574,16 +574,6 @@ extension AstNode {
         }
     }
 
-    var asCheckedAssign: Checker.Assign {
-        get {
-            assert(kind == Checker.Assign.astKind)
-            return value as! Checker.Assign
-        }
-        set {
-            value = newValue
-        }
-    }
-
     var asCheckedBlock: Checker.Block {
         get {
             assert(kind == Checker.Block.astKind)
@@ -758,16 +748,6 @@ extension AstNode {
         get {
             assert(kind == Checker.StringLiteral.astKind)
             return value as! Checker.StringLiteral
-        }
-        set {
-            value = newValue
-        }
-    }
-
-    var asCheckedSwitch: Checker.Switch {
-        get {
-            assert(kind == Checker.Switch.astKind)
-            return value as! Checker.Switch
         }
         set {
             value = newValue