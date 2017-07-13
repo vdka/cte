9f5a97cafea1bc2204f805d455b1153ead56813b
Brett R. Toomey <brettcwx@gmail.com>
2017-07-13 22:09:06 +0200

    Broke compiler into modules to prepare for tests

---
+++ Sources/Core/CompilerOptions.swift
@@ -0,0 +1,42 @@
+
+public struct Options: OptionSet {
+    public static var instance: Options!
+
+    public let rawValue: UInt16
+
+    public static let noCleanup    = Options(rawValue: 0b0000_0001)
+
+    public static let emitTiming   = Options(rawValue: 0b0001_0000)
+    public static let emitIr       = Options(rawValue: 0b0010_0000)
+    public static let emitBitcode  = Options(rawValue: 0b0100_0000)
+    public static let emitAssembly = Options(rawValue: 0b1000_0000)
+
+    // why do I have to do this?...
+    public init(rawValue: UInt16) {
+        self.rawValue = rawValue
+    }
+
+    public static func from(arguments: ArraySlice<String>) -> Options {
+        var value: Options = []
+
+        for arg in arguments {
+
+            switch arg {
+            case "-no-cleanup":
+                value.insert(.noCleanup)
+            case "-emit-timing":
+                value.insert(.emitTiming)
+            case "-emit-ir":
+                value.insert(.emitIr)
+            case "-emit-bitcode":
+                value.insert(.emitBitcode)
+            case "-emit-asm", "-S":
+                value.insert(.emitAssembly)
+            default:
+                print("WARNING: argument unused during compilation: '\(arg)'")
+            }
+        }
+
+        return value
+    }
+}
